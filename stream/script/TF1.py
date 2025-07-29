8a6ed7ef9e66744157e1daded3815f53ee46e358
RodriIPTV6 <Gamingrodrigo2009@hotmail.com>
2025-07-29 12:49:08 +0100

    Update TF1.py

--- stream/script/TF1.py
+++ stream/script/TF1.py
@@ -6,6 +6,7 @@ import re
 def fetch_m3u8_from_page(url):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        "Referer": "https://www.stream4free.tv/"
     }
     try:
         response = requests.get(url, headers=headers, timeout=10)
